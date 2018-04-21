d914df12d652875fae5102cfe5aeaccc6f895f83
T-Shimozaki <takehiroshimozaki@gmail.com>
2018-04-21 22:06:25 +0900

    test01
    commit

---
+++ 入門編/test/test01.py
@@ -0,0 +1,13 @@
+test_str="python"
+test_str=test_str+"-"
+test_str=test_str+"izm"
+print(test_str)
+
+test_str2="012"
+test_str2+="345"
+test_str2+="678"
+test_str2+="9"
+print(test_str2)
+
+test_str3="012"*3
+print(test_str3)
